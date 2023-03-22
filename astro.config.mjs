64be8dd67b8c757fba4693b97d012ac6fa73bba6
Coding & Fitness <boadude@gmail.com>
2023-03-22 13:03:04 -0300

    Version 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   output: "server",
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
