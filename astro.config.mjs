bf1d12b913bb868560d70cf9005215b7a70ad4d3
Coding & Fitness <boadude@gmail.com>
2023-04-06 15:24:50 -0400

    feat(side project page): creado la página de side projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
-import image from "@astrojs/image"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), alpinejs()],
   output: "server",
-  adapter: vercel(),
-})
+  adapter: vercel()
+});
